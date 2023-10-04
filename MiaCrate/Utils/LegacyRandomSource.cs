ab31f60d96fc72b6a0970ec941478cc9d11aa31c
KaKusaOAO <ka.kusaowo@gmail.com>
2023-10-04 08:11:58 +0800

    MiaCrate: Fixed issues in `LegacyRandomSource`

--- MiaCrate/Utils/LegacyRandomSource.cs
+++ MiaCrate/Utils/LegacyRandomSource.cs
@@ -2,6 +2,11 @@ namespace MiaCrate;
 
 public class LegacyRandomSource : IBitRandomSource
 {
+    private const int ModulusBits = 48;
+    private const long ModulusMask = 281474976710655L;
+    private const long Multiplier = 25214903917L;
+    private const long Increment = 11;
+    
     private long _seed;
     private readonly MarsagliaPolarGaussian _gaussianSource;
     private readonly SemaphoreSlim _seedLock = new(1, 1);
@@ -19,7 +24,7 @@ public class LegacyRandomSource : IBitRandomSource
     public void SetSeed(long seed)
     {
         var expected = Interlocked.Read(ref _seed);
-        if (Interlocked.CompareExchange(ref _seed, (seed ^ 25214903917L) & 281474976710655L, expected) != expected) 
+        if (Interlocked.CompareExchange(ref _seed, (seed ^ Multiplier) & ModulusMask, expected) != expected) 
             throw new Exception("?");
         _gaussianSource.Reset();
     }
@@ -29,10 +34,10 @@ public class LegacyRandomSource : IBitRandomSource
     public int NextBits(int bits)
     {
         var seed = Interlocked.Read(ref _seed);
-        var val = (seed ^ 25214903917L) & 281474976710655L;
+        var val = (seed * Multiplier + Increment) & ModulusMask;
         if (Interlocked.CompareExchange(ref _seed, val, seed) != seed) 
             throw new Exception("?");
         
-        return (int) (val >> 48 - bits);
+        return (int) (val >> ModulusBits - bits);
     }
 }
